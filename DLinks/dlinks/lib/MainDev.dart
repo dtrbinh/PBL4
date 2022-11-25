c1c93faf8888be8d007a70181d11d592e700df7d
dotranbinh <binhdo@wakumo.vn>
2022-11-25 17:34:56 +0700

    feat: config flavor

---
+++ DLinks/dlinks/lib/MainDev.dart
@@ -0,0 +1,31 @@
+import 'package:dlinks/features/DLinksApplication.dart';
+import 'package:dlinks/utils/CallbackFunction.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+
+import 'data/model/LifeCycleEventHandler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  WidgetsBinding.instance.addObserver(LifecycleEventHandler(
+    detachedCallBack: () async {
+      await setChatUserOffline();
+    },
+    resumeCallBack: () async {
+      await setChatUserOnline();
+    },
+  ));
+
+  await FlutterDownloader.initialize(
+      debug: true,
+      ignoreSsl:
+          true // option: set to false to disable working with http links (default: false)
+      );
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(const DLinksApplication());
+}
