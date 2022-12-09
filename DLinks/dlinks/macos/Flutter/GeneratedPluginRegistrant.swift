3c47daaa0211e1934202019675ced5240c4548ab
Do Tran Binh <dotranbinhqng02@gmail.com>
2022-12-09 21:40:33 +0700

    feat: speech to text message

--- DLinks/dlinks/macos/Flutter/GeneratedPluginRegistrant.swift
+++ DLinks/dlinks/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import just_audio
 import path_provider_macos
 import shared_preferences_macos
+import speech_to_text_macos
 import sqflite
 import url_launcher_macos
 
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
