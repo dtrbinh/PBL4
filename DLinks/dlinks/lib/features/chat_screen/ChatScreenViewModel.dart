e1364c947c25818a2129d858c13c6db6c0411e5f
Do Tran Binh <dotranbinhqng02@gmail.com>
2022-12-30 18:52:55 +0700

    fix: some typo

--- DLinks/dlinks/lib/features/chat_screen/ChatScreenViewModel.dart
+++ DLinks/dlinks/lib/features/chat_screen/ChatScreenViewModel.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:math';
-import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
